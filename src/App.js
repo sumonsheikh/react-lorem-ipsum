a72cb46c2e6b23f6c007559f29c3f1d88d893d5d
sumonsheikh <sm.sumon117@gmail.com>
2020-12-18 10:00:50 +0600

    generate paragraph after clicking the button

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import data from './data';
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
-  const handleSubmit = () =>{
-    
+  const handleSubmit = (e) =>{
+   e.preventDefault();
+   let amount = parseInt(count);
+   if(amount <= 0){
+     amount = 1;
+   }
+   if(amount > 8){
+     amount = 8;
+   }
+    setText(data.slice(0,amount));
   }
+
   return (
    <section className="section-center">
      <h3>Tired of boaring lorem ipsum?</h3>
@@ -24,6 +33,13 @@ function App() {
           generate
         </button>
      </form>
+     <article className="lorem-text">
+         {text.map((item,index) =>{
+           return(
+             <p key={index}>{item}</p>
+           );
+         })}
+     </article>
    </section>
   );
 }
