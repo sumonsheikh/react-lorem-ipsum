299fb990e88d85b0bb259750ade86b4a51228ced
sumonsheikh <sm.sumon117@gmail.com>
2020-12-18 09:32:06 +0600

    markup done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-
+import React, {useState} from 'react';
+import data from './data';
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const handleSubmit = () =>{
+    
+  }
   return (
-   
+   <section className="section-center">
+     <h3>Tired of boaring lorem ipsum?</h3>
+     <form className="lorem-form" onSubmit={handleSubmit}>
+       <label htmlFor="amount">paragraph: </label>
+       <input 
+          type="number" 
+          name="amount" 
+          id="amount" 
+          value={count} 
+          onChange={(e)=>setCount(e.target.value)}
+        />
+        <button 
+          type="submit" 
+          className="btn-generate">
+          generate
+        </button>
+     </form>
+   </section>
   );
 }
 
